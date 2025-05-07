0188a0dc89290cf19c39337d8899ebec966033f3
CharlotteBCK <charlotte44300@gmail.com>
2025-05-07 14:57:32 +0200

    responsive

--- app/components/homeScreen/SpotCard.tsx
+++ app/components/homeScreen/SpotCard.tsx
@@ -1,12 +1,9 @@
-import React, { useEffect, useState } from 'react';
-import { View, Text, Image, FlatList, StyleSheet, Dimensions, ActivityIndicator } from 'react-native';
-import airtableService from '@/airtableService';
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
 import type { AirtableRecord } from '@/airtableService';
 import style from '@/styles/global';
 import SpotCardImage from './SpotCardImage';
 import SpotCardDetails from './SpotCardDetails';
-
-const { width } = Dimensions.get('window');
 
 interface SpotCardProps {
   spot: AirtableRecord;
@@ -22,26 +19,18 @@ const SpotCard = ({ spot }: SpotCardProps) => {
 };
 
 const styles = StyleSheet.create({
-  listContent: {
-    paddingBottom: 20,
-    paddingHorizontal: 10,
-  },
   card: {
-    width: width * 0.9,
-    marginVertical: 30,
-    // marginHorizontal: 10,
+    width: '100%',
     padding: 20,
     backgroundColor: style.color?.primary || '#f0f0f0',
     borderRadius: 12,
     alignItems: 'center',
-    alignSelf: 'center',
     shadowColor: '#000',
     shadowOffset: { width: 10, height: 2 },
     shadowOpacity: 0.1,
     shadowRadius: 5,
     elevation: 3, // pour Android
   },
-  
 });
 
 export default SpotCard;
